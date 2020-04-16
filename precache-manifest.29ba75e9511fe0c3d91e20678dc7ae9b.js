6c829ae2f42e775ed60f383c78f84807626f41ec
John Zhou <john2001@live.ca>
2020-04-16 12:34:13 -0400

    Updates

---
+++ precache-manifest.29ba75e9511fe0c3d91e20678dc7ae9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9b5615e7efa35b56b08f5d4d008eac",
+    "url": "/Simple-Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "8b9ee8ae19fc2c232b19",
+    "url": "/Simple-Tic-Tac-Toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "38c58a404a3fb46dd5f4",
+    "url": "/Simple-Tic-Tac-Toe/static/js/2.b102e267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Tic-Tac-Toe/static/js/2.b102e267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b9ee8ae19fc2c232b19",
+    "url": "/Simple-Tic-Tac-Toe/static/js/main.dd4c5c67.chunk.js"
+  },
+  {
+    "revision": "f51a8ecdaca221a7a1af",
+    "url": "/Simple-Tic-Tac-Toe/static/js/runtime-main.b2dda990.js"
+  }
+]);
